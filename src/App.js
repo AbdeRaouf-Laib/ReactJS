88b4f6b4be548cb5e96856d4e665e94b7d2fd59a
joessi <yocefyouyou31@gmail.com>
2023-11-05 11:41:25 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          source code at <code>https://github.com/AbdeRaouf-Laib/ReactJS.git</code>
         </p>
         <a
           className="App-link"
